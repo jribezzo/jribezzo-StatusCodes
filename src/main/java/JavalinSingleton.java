0e9c45bc1d422fec7c451e4c7b99a768837df4c7
jribezzo <Jesseribezzo@gmail.com>
2023-02-22 21:36:24 +0000

    done

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -7,7 +7,7 @@ public class JavalinSingleton {
      * Assignment: return the status code for a generic "client side error" in the http response
      *
      * You will not need to run app.start in this method. The test cases, or main method, will do this for you - this
-     * method only needs to return a properly configured Javalin Server, represented by the 'app' object created below.
+     * method onl y needs to return a properly configured Javalin Server, represented by the 'app' object created below.
      *
      * Please refer to the "StatusCodes.MD" file.
      */
